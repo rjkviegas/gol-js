24aac4491c9c338b5c4645489362c46f7b8e7f32
Ryan Viegas <rjkviegas@gmail.com>
2020-09-20 17:48:48 +0100

    test countNeighbours for 1 live elemenent

--- spec/count_neighbours_spec.js
+++ spec/count_neighbours_spec.js
@@ -2,12 +2,14 @@ const { countNeighbours } = require("../src/count_neighbours")
 
 describe("countNeighbours", function() {
     describe("returns total live neighbours", function() {
-        it("for cell with no live neighbours in 3x3 grid", function() {
-            const grid = [[0, 0, 0],
-                          [0, 0, 0],
-                          [0, 0, 0]];
-            const cell = grid[1][1];
-            expect(countNeighbours(cell)).toEqual(0);
+        it("for cell with no live neighbours", function() {
+            const cell = [0];
+            expect(countNeighbours(cell, cell)).toEqual(0);
+        })
+        it("for array with 1 live element", function() {
+            const pair = [1, 0];
+            expect(countNeighbours(pair, pair[0])).toEqual(0);
+            expect(countNeighbours(pair, pair[1])).toEqual(1);
         })
     })
 })
--- src/count_neighbours.js
+++ src/count_neighbours.js
@@ -1,5 +1,15 @@
-function countNeighbours(grid) {
-    return 0;
+function countNeighbours(array, cell) {
+    let result = 0;
+    if (array.length === 1) {
+        return result;
+    }
+    array.forEach(function(e) {
+        if (e === 1) {
+            result += 1;
+        }
+    })
+    return result - cell;
+
 }
 
 module.exports = { countNeighbours }
