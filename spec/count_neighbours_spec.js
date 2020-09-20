772da22de79a792349bbee124dcdfcc38a6d63b0
Ryan Viegas <rjkviegas@gmail.com>
2020-09-20 18:07:56 +0100

    test countNeighbours 3 element array 2 live elements

--- spec/count_neighbours_spec.js
+++ spec/count_neighbours_spec.js
@@ -2,20 +2,28 @@ const { countNeighbours } = require("../src/count_neighbours")
 
 describe("countNeighbours", function() {
     describe("returns total live neighbours", function() {
-        it("for cell with no live neighbours", function() {
-            const cell = [0];
-            expect(countNeighbours(cell, 0)).toEqual(0);
-        })
-        it("for array of length 2 with 1 live element", function() {
-            const pair = [1, 0];
-            expect(countNeighbours(pair, 0)).toEqual(0);
-            expect(countNeighbours(pair, 1)).toEqual(1);
-        })
-        it("for array with length 3 with 1 live element", function() {
-            const four = [1, 0, 0];
-            expect(countNeighbours(four, 0)).toEqual(0);
-            expect(countNeighbours(four, 1)).toEqual(1);
-            expect(countNeighbours(four, 2)).toEqual(0);
+        describe("for single array", function() {
+            it("1 element", function() {
+                const cell = [0];
+                expect(countNeighbours(cell, 0)).toEqual(0);
+            })
+            it("of length 2 with 1 live element", function() {
+                const pair = [1, 0];
+                expect(countNeighbours(pair, 0)).toEqual(0);
+                expect(countNeighbours(pair, 1)).toEqual(1);
+            })
+            it("of length 3 with 1 live element", function() {
+                const arr = [1, 0, 0];
+                expect(countNeighbours(arr, 0)).toEqual(0);
+                expect(countNeighbours(arr, 1)).toEqual(1);
+                expect(countNeighbours(arr, 2)).toEqual(0);
+            })
+            it("of length 3 with 2 live element", function() {
+                const arr = [0, 1, 1];
+                expect(countNeighbours(arr, 0)).toEqual(1);
+                expect(countNeighbours(arr, 1)).toEqual(1);
+                expect(countNeighbours(arr, 2)).toEqual(1);
+            })
         })
     })
 })
