3016b764f6cdba03ae1a5e2b0458d746acb5e380
Ryan Viegas <rjkviegas@gmail.com>
2020-09-20 17:11:07 +0100

    grid elements initialize to 0

--- spec/grid_spec.js
+++ spec/grid_spec.js
@@ -6,10 +6,17 @@ describe("grid", function() {
             let rows = 10;
             let cols = 8;
             const grid = setUp(rows, cols);
-        expect(grid).toBeInstanceOf(Array);
-        expect(grid.length).toEqual(rows);
-        expect(grid[0]).toBeInstanceOf(Array);
-        expect(grid[0].length).toEqual(cols);
+            expect(grid).toBeInstanceOf(Array);
+            expect(grid.length).toEqual(rows);
+            expect(grid[0]).toBeInstanceOf(Array);
+            expect(grid[0].length).toEqual(cols);
+        });
+        it("all elements equal 0", function() {
+            const grid = setUp(10, 8);
+            const elementsTotal = grid.flat().reduce(function(accumulator, currentValue) {
+                return accumulator + currentValue;
+            });
+            expect(elementsTotal).toEqual(0);
+        })
     });
-  });
 });
--- src/grid.js
+++ src/grid.js
@@ -1,7 +1,7 @@
 function setUp(rows, cols) {
     const result = new Array(rows);
     for (let i = 0; i < result.length; i += 1) {
-        result[i] = new Array(cols);
+        result[i] = new Array(cols).fill(0);
     }
     return result;
 }
